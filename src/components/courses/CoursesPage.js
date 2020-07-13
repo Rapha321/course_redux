cd275963ba102f7096e887c1de6a3bd209eeb5d1
Raphael Albern <ralpha19@hotmail.com>
2020-07-12 20:56:36 -0400

    set up redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
     return (
       <>
-        <h2>Courses</h2>
-        <p>Knowledge is power</p>
+        <form onSubmit={this.handleSubmit}>
+          <h2>Courses</h2>
+          <h3>Add courses</h3>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.course.title}
+          />
+          <input type="submit" value="Save" />
+        </form>
       </>
     );
   }
